e7f53e03c1d197e8fb49bc13bda8b0da30d441c2
Neil McDonald <neilmcdonald@cirrusnova.com>
2025-07-29 17:18:11 +0100

    x

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataverseContactMerger")]
+[assembly: AssemblyTitle("DataverseMerger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataverseContactMerger")]
+[assembly: AssemblyProduct("DataverseMerger")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
